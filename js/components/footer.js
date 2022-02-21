8a88ea37d6119e0f9b9778bf5b6491f0d43c13b4
Angel Mazariegos <mazariegosnangelece@gmail.com>
2022-02-21 17:09:53 -0600

    cambios

---
+++ js/components/footer.js
@@ -0,0 +1,10 @@
+export default Vue.component("footer", {
+  props: ["onKeyup"],
+  template: `
+    <footer class="bg-dark py-5">
+    <div class="container text-white">
+      <p class="text-center m-0">Copyright &copy; 2022</p>
+    </div>
+  </footer>
+      `,
+});
