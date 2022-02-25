fa480817a63073ef0edb2d77cd6d41f91bd2f7a7
Nicolle Santizo <idiego210115@gmail.com>
2022-02-24 19:03:07 -0800

    vue inicio

--- js/app.js
+++ js/app.js
@@ -1,17 +1,11 @@
 import LoginPage from "./pages/login.js";
 import RegisterPage from "./pages/registro.js";
-// import ProductsPage from "./pages/products_page.js";
-// import EditProductPage from "./pages/edit_product_page.js";
-// import CategoriesPage from "./pages/categories_page.js";
+import inicio from "./pages/inicio.js";
 import Navbar from "./components/navbar.js";
-// import AddProduct from "./pages/add_product_page.js";
 var routes = [
-  { path: "/", component: LoginPage },
+  { path: "/", component: inicio},
+  { path: "/login", component: LoginPage },
   { path: "/register", component: RegisterPage },
-  // { path: "/products", component: ProductsPage },
-  // { path: "/products/edit", component: EditProductPage },
-  // { path: "/categories", component: CategoriesPage },
-  // { path: "/add/product", component: AddProduct },
 ];
 var router = new VueRouter({
   routes: routes,
--- js/components/navbar.js
+++ js/components/navbar.js
@@ -30,83 +30,52 @@ export default Vue.component("navbar", {
   },
   template: `
     <nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
-        <div class="container-fluid px-5">
-          <a href="/index.html" class="navbar-brand">
-            <h5 class="">YourLifeWithUs</h5>
-          </a>
-          <a href="/index.html" class="navbar-brand">
-            <img src="/img/Logo.png" alt="YLWU" width="100" />
-          </a>
-          <button
-            class="navbar-toggler"
-            role="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbar_menu"
-            aria-controls="navbar_menu"
-            aria-expanded="false"
-            aria-label="Toggle"
-          >
-            <span class="navbar-toggler-icon"> </span>
-          </button>
-  
-          <div class="navbar-collapse collapse" id="navbar_menu">
-            <ul class="navbar-nav me-auto">
-              <li class="nav-item">
-                <a href="/index.html" class="nav-link active">Inicio</a>
-              </li>
-              <li class="nav-item">
-                <a href="/pages/nosotros.html" class="nav-link">Nosotros</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a
-                  href="/pages/tienda.html"
-                  class="nav-link dropdown-toggle"
-                  id="drop_products"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                  >Tienda</a
-                >
-                <ul class="dropdown-menu" aria-labelledby="drop_products">
-                  <li>
-                    <a href="/pages/tienda.html" class="dropdown-item">Todos</a>
-                  </li>
-                  <li>
-                    <hr class="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a href="/pages/tienda.html" class="dropdown-item">Medicamentos</a>
-                  </li>
-                  <li>
-                    <a href="/pages/tienda.html" class="dropdown-item">Remedios</a>
-                  </li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a href="/pages/contact.html" class="nav-link">Contactanos</a>
-              </li>
-            </ul>
-          </div>
-  
-          <form class="d-flex">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Buscar..."
-            />
-            <button class="btn btn-outline-secondary me-4" type="submit">
-              Ir
+      <div class="container px-5">
+        <!--LOGO-->
+        <a href="/index.html" class="navbar-brand">
+          <h5 class="">YourLifeWithUs</h5>
+        </a>
+        <a href="/index.html" class="navbar-brand">
+          <img src="/images/logo.png" alt="..." width="100" />
+        </a>
+        <!--TOGGLER (BARRITAS MOBILE)-->
+        <button
+          class="navbar-toggler"
+          role="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbar_menu"
+          aria-controls="navbar_menu"
+          aria-expanded="false"
+          aria-label="Toggle"
+        >
+          <span class="navbar-toggler-icon"> </span>
+        </button>
+
+        <!--MENU DE NAVEGACION-->
+        <div class="navbar-collapse collapse" id="navbar_menu">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+              <a href="../index.html" class="nav-link active">Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a href="/pages/tienda.html" class="nav-link">Medicamentos</a>
+            </li>
+            <li class="nav-item">
+              <a href="/pages/tienda.html" class="nav-link">Remedios</a>
+            </li>
+            <li class="nav-item">
+              <a href="/pages/contact.html" class="nav-link">Contactanos</a>
+            </li>
+          </ul>
+        </div>
+        <!--LISTA DE ITEMS-->
+        <div class="d-flex">
+          <a href="/pages/compra.html">
+            <button id="btn_cart" class="btn btn-outline-secondary mx-4">
+              <i class="bi bi-cart-fill"></i>
             </button>
-          </form>
-  
-          <div class="d-flex">
-            <a href="/pages/compra.html">
-              <button id="btn_cart" class="btn btn-outline-secondary mx-4">
-                <i class="bi bi-cart-fill"></i>
-                <span class="badge rounded-pill bg-secondary ms-1">0</span>
-              </button>
-            </a>
-          </div>
+          </a>
+        </div>
   
           <div class="d-flex">
               <a href="#" >
